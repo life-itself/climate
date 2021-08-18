25647e92cff7d30ba046b8350e001a540c33f3ca
Rufus Pollock <rufus.pollock@datopian.com>
2021-08-18 23:54:00 +0200

    [site/home][s]: use content/home on front page.

--- site/pages/index.js
+++ site/pages/index.js
@@ -1,17 +1,49 @@
-import Layout from '../components/Layout'
+import fs from 'fs'
+import path from 'path'
 
-export default function Home() {
+import Layout from '../components/Layout'
+import Link from 'next/link'
+
+import parse from '../lib/markdown.js'
+
+import { MDXRemote } from 'next-mdx-remote'
+import dynamic from 'next/dynamic'
+import CustomLink from '../components/CustomLink'
+
+const components = {
+  a: CustomLink,
+}
+
+
+export default function Home({ source, frontMatter }) {
   return (
     <Layout title="home">
       <div className="flex flex-col items-center justify-center min-h-screen py-2 flex-1 px-20 w-full text-center">
         <h1 className="text-6xl font-bold">
           Life Itself Climate Inquiry
         </h1>
-
         <p className="mt-3 text-2xl">
           Life Itself's ongoing inquiry into the climate crisis 🌍🔥 
         </p>
       </div>
+      <article className="prose mx-auto p-6">
+        <MDXRemote {...source} components={components} />
+      </article>
     </Layout>
   )
 }
+
+export const getStaticProps = async () => {
+  const postFilePath = path.join('..', 'content', 'home.md')
+  const source = fs.readFileSync(postFilePath)
+
+  const { mdxSource, frontMatter } = await parse(source)
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: frontMatter,
+    },
+  }
+}
+
