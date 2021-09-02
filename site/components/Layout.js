a6504245c5e1d71f663c4575f914f0eb66f2b163
Rufus Pollock <rufus.pollock@datopian.com>
2021-09-02 13:38:11 +0200

    [site/theme][xs]: title now shows page title before site title (rather than other way round).

--- site/components/Layout.js
+++ site/components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children, title = 'Home' }) {
   return (
     <>
       <Head>
-        <title>Life Itself Climate Inquiry 🌍🔥 - {title}</title>
+        <title>{title}{title && '- '}Life Itself Climate Inquiry 🌍🔥</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🔥</text></svg>" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
