8a486287801713d6034ab2dd116b77a07cb30052
Rufus Pollock <rufus.pollock@datopian.com>
2021-10-31 13:21:31 +0100

    [site/theme][s]: add chatwoot bot so that people have a way to get in contact.

--- site/pages/_app.js
+++ site/pages/_app.js
@@ -48,7 +48,27 @@ function MyApp({ Component, pageProps }) {
           }}
         />
         }
-    <Component {...pageProps} />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(d,t) {
+            var BASE_URL="https://app.chatwoot.com";
+            var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=BASE_URL+"/packs/js/sdk.js";
+            g.defer = true;
+            g.async = true;
+            s.parentNode.insertBefore(g,s);
+            g.onload=function(){
+              window.chatwootSDK.run({
+                websiteToken: 'tc1GJE9wAmNSHGVUUa8gDLcd',
+                baseUrl: BASE_URL
+              })
+            }
+          })(document,"script");
+          `
+        }}
+        />
+      <Component {...pageProps} />
   </>
   )
 }
